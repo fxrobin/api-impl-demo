6888c5d8cbd6d6f8384c9d2e3024616e23e66ffb
François-Xavier Robin <fxrobin@gmail.com>
2021-01-27 18:30:44 +0100

    small fix.

--- src/main/java/fr/fxjavadevblog/aid/metadata/VideoGamePagedResponse.java
+++ src/main/java/fr/fxjavadevblog/aid/metadata/VideoGamePagedResponse.java
@@ -3,11 +3,9 @@ package fr.fxjavadevblog.aid.metadata;
 import java.util.List;
 
 import fr.fxjavadevblog.aid.api.videogame.VideoGame;
-import fr.fxjavadevblog.aid.utils.pagination.PagedMetadata;
 import fr.fxjavadevblog.aid.utils.pagination.PagedResponse;
 
 // only for generating documentation :-(
-@SuppressWarnings("unused")
 public class VideoGamePagedResponse extends PagedResponse<List<VideoGame>> {
 
 }
